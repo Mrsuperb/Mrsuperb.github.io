ae1f16abaeb8a8fd7634e6f4e6597078673891a6
Mrsuperb <934627374@qq.com>
2019-09-11 17:56:40 +0800

    Site updated: 2019-09-11 17:56:37

--- js/wenzi.js
+++ js/wenzi.js
@@ -1,10 +1,12 @@
-<script type="text/javascript">
-/* 鼠标特效 */
+
+
+
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
+        // var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
         var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-        var $i = $("<span/>").text(a[a_idx]);
+        var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
@@ -14,7 +16,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#ff6651"
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
         });
         $("body").append($i);
         $i.animate({
@@ -27,4 +29,3 @@ jQuery(document).ready(function($) {
         });
     });
 });
-</script>
